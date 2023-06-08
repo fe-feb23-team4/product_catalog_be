81903a05e9ba5820bc1c23c183dd6fd659634be4
Aleksandr <alexnovaklawyer@gmail.com>
2023-06-08 22:01:28 +0300

    build version added

---
+++ build/utils/dbinit.js
@@ -0,0 +1,16 @@
+'use strict';
+Object.defineProperty(exports, '__esModule', { value: true });
+exports.dbinit = void 0;
+
+/* eslint-disable max-len */
+const sequelize_typescript_1 = require('sequelize-typescript');
+const URI
+  = 'postgres://alexnovaklawyer:4KnxRozeh1mF@ep-billowing-cake-723225.eu-central-1.aws.neon.tech/neondb';
+const dbinit = () =>
+  new sequelize_typescript_1.Sequelize(URI, {
+    dialectOptions: {
+      ssl: true,
+    },
+  });
+
+exports.dbinit = dbinit;
--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,12 @@
 import express from 'express';
 import cors from 'cors';
+import { dbinit } from './utils/dbinit';
 
 const PORT = process.env.PORT || 3000;
 
 const server = express();
+
+dbinit();
 
 server.use(cors());
 
