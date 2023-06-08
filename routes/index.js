57a97428fcd79969ba94fdc96ab4e91d39019e3e
Aleksandr <alexnovaklawyer@gmail.com>
2023-06-08 15:51:30 +0300

    eslint, prettier, husky, lint staged added

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,11 @@
-var express = require('express');
-var router = express.Router();
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import { Router } from 'express';
+
+const router = Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', function(req, res, _next) {
   res.render('index', { title: 'Express' });
 });
 
-module.exports = router;
+export default router;
--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,11 @@
-var express = require('express');
-var router = express.Router();
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import { Router } from 'express';
+
+const router = Router();
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
+router.get('/', function(req, res, _next) {
   res.send('respond with a resource');
 });
 
-module.exports = router;
+export default router;
