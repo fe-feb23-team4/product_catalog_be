b92530cd2a90fd292e02ee4652eb4fb1375b6b3c
Aleksandr <alexnovaklawyer@gmail.com>
2023-06-09 13:06:24 +0300

    config files added

--- src/server.ts
+++ src/server.ts
@@ -1,3 +1,4 @@
+/* eslint-disable no-console */
 import express from 'express';
 import cors from 'cors';
 import { dbinit } from './utils/dbinit';
@@ -5,6 +6,8 @@ import { dbinit } from './utils/dbinit';
 const PORT = process.env.PORT || 3000;
 
 const server = express();
+
+console.log();
 
 dbinit();
 
