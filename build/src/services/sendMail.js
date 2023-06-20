a6b3cbcb602a10f0053c9baae65a41e574231a80
Aleksandr <alexnovaklawyer@gmail.com>
2023-06-20 14:25:44 +0300

    build updated

--- build/src/services/sendMail.js
+++ build/src/services/sendMail.js
@@ -3,7 +3,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 require("dotenv/config");
 const emailService_1 = require("./emailService");
 (0, emailService_1.send)({
-    email: 'pashamalyshkin2003@gmail.com',
+    email: 'test@gmail.com',
     subject: 'Proposition',
-    html: `Privet Pasha! Eto Myla. Ti takoy ahuenniy team leader. Priglasi menya na cofee. Chmok-chmok`,
+    html: `Registration link`,
 });
