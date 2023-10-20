12a3ff22652825216e109de49a166bca9b918fa1
Oleksandr_Novak <asa.novak@cryptacash.company>
2023-10-20 17:03:56 +0300

    Phone test added

--- src/server.ts
+++ src/server.ts
@@ -8,6 +8,8 @@ import productRouter from './routes/productRouter';
 import authRouter from './routes/authRouter';
 import userRouter from './routes/userRouter';
 import cookieParser from 'cookie-parser';
+import fs from 'fs';
+import { json } from 'sequelize';
 
 dotenv.config();
 
@@ -31,6 +33,76 @@ server.get('/', (req, res) => {
     Main end points are /products, /phones.\n
     Also, you can fetch an image. You can find all the URLs in database`,
   );
+});
+
+server.post('/auth', async(req, res) => {
+  const { phone_number } = req.body;
+
+  if (!phone_number) {
+    res.sendStatus(400);
+  }
+
+  const myHeaders = new Headers();
+
+  myHeaders.append("Authorization", "Bearer 770fd644f280e853573c9351617694c01412");
+  myHeaders.append("Content-Type", "application/json");
+
+  const raw = JSON.stringify({
+    "phone_number": phone_number,
+    "options": {
+    "number_length": null,
+    "send_result": true,
+    "callback_url": "https://product-catalog-be-s8k7.onrender.com/phoneConfirmed",
+    "callback_key": null
+    }
+  });
+
+  const requestOptions: RequestInit = {
+    method: 'POST',
+    headers: myHeaders,
+    body: raw,
+    redirect: 'follow'
+  };
+
+  const result = await fetch('https://call2fa.rikkicom.net/call_api/call', requestOptions);
+
+  res.status(200);
+  res.send(result);
+});
+
+server.post('/phoneConfirmed', async(req, res) => {
+  const dataPath = path.join(__dirname, 'clientBase.json');
+  const rawData = fs.readFileSync(dataPath, 'utf-8');
+  const jsonData = JSON.parse(rawData);
+
+  jsonData.clients.push(req.body);
+  const jsonNewData = JSON.stringify(jsonData);
+
+  fs.writeFileSync(jsonNewData, 'clientBase.json');
+});
+
+server.post('/phoneCheck', (req, res) => {
+  const { call_id } = req.body;
+
+  const dataPath = path.join(__dirname, 'clientBase.json');
+  const rawData = fs.readFileSync(dataPath, 'utf-8');
+  const jsonData = JSON.parse(rawData);
+  let checker = false;
+
+  for (const elem of jsonData.clients) {
+    if (elem.call_id === call_id) {
+      checker = true;
+    }
+  }
+
+  if (checker) {
+    res.status(200);
+    res.send({
+      message: 'ok'
+    });
+  } else {
+    res.sendStatus(403);
+  }
 });
 
 server.use(authRouter.authRouter);
